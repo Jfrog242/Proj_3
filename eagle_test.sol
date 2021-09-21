4c9680e43c134c97d38659861da0614b7fdcb114
Jfrog242 <80180919+Jfrog242@users.noreply.github.com>
2021-09-20 21:25:35 -0500

    Test contract

---
+++ eagle_test.sol
@@ -0,0 +1,22 @@
+
+pragma solidity ^0.5.2;
+
+import "https://github.com/ProjectOpenSea/opensea-creatures/blob/master/contracts/ERC721Tradable.sol";
+
+/**
+ * @title Creature
+ * Creature - a contract for my non-fungible creatures.
+ */
+contract Creature is ERC721Tradable {
+    constructor(address _proxyRegistryAddress)
+        ERC721Tradable("Eddie_eagle", "EE", _proxyRegistryAddress)
+    {}
+
+    function baseTokenURI() override public pure returns (string memory) {
+        return "https://creatures-api.opensea.io/api/creature/";
+    }
+
+    function contractURI() public pure returns (string memory) {
+        return "https://creatures-api.opensea.io/contract/opensea-creatures";
+    }
+}
